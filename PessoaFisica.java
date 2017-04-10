c6522201ac921ea5e265ca16e42bb93de0a16eeb
BrunoRodri <fidbruno@hotmail.com>
2017-04-09 23:56:33 -0300

    Add files via upload

---
+++ PessoaFisica.java
@@ -0,0 +1,34 @@
+public class PessoaFisica extends Cliente{
+
+	private String cpf;
+	private int valImovel;
+	private String zona;
+	private String tipo;
+	
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getValImovel() {
+		return valImovel;
+	}
+	public void setValImovel(int valImovel) {
+		this.valImovel = valImovel;
+	}
+	public String getZona() {
+		return zona;
+	}
+	public void setZona(String zona) {
+		this.zona = zona;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+}
---
+++ Seguradora.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class Seguradora {
+	
+	Scanner ler = new Scanner(System.in);
+	public int tipoCliente;
+	public String nome;
+	
+	public void imprimeMenu(){
+		System.out.println("===========SEGURADORA SE FUDEU==========");
+		System.out.println("1- Cadastra Cliente");
+		System.out.println("2- Cadastrar Contrato");
+		System.out.println("3- Listar Cliente");
+		System.out.println("4- Listar Contrato");
+		System.out.println("5- Cadastrar Sinistro");
+		System.out.println("6- Listar Sinistro");
+		System.out.println("7- Listar Contratos sem Sinistro");
+		System.out.println("8- Persistencia com Serializacao");
+		System.out.println("0- Sair");
+	
+		System.out.println("Entre com uma opcÃ£o: ");
+	}
+	public int tipoCliente(){
+		int option;
+		option = 0;
+		while (option !=1 && option !=2 ){
+			System.out.println("Pessoa Física [1] ou Pessoa Jurídica [2]");
+			option = ler.nextInt();
+			if (option == 1){
+				return option;
+			}
+			else{ 
+				return option;
+			}
+			
+		}
+		
+	} 
+	public void main(String[] args) {
+		
+		int opcao;
+		Scanner ler = new Scanner(System.in);
+		while (opcao != 0){
+			imprimeMenu();
+			opcao = ler.nextInt();
+		
+			if (opcao == 1){
+				int option;
+				tipoCliente();
+				if (option == 1){
+					//cadastroFisica
+				}
+				else{
+					//cadastroJURIDICA
+				}
+			}
+			if (opcao == 2){
+			
+			}
+			if (opcao == 3){
+			
+			}
+			if (opcao == 4){
+			
+			}
+			if (opcao == 5){
+			
+			}
+			if (opcao == 6){
+			
+			}
+			if (opcao == 7){
+		
+			}
+			if (opcao == 8){
+		
+			}
+			if (opcao < 0 || opcao > 8){
+				System.out.println("Opção inválida, por favor, tente novamente!");
+			}
+			else {
+				break;
+			}
+			System.out.println("Digite seu nome: ");
+			nome = ler.nextLine();
+			}
+
+	}
+
+}
